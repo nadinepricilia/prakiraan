4917d03dde17c21ab7c74d1b7864b692ad6d8ed1
Muhammad Aswan Syahputra <muhammadaswansyahputra@gmail.com>
2019-02-20 23:00:06 +0700

    add query_ncss

---
+++ R/get_ncss.R
@@ -0,0 +1,29 @@
+#' Antarmuka The NetCDF Subset Service (NCSS)
+#'
+#' Fungsi antarmuka dari NetCDF Subset Service
+#'
+#' @param host nama peladen, misalnya thredds.ucar.edu
+#' @param context "thredds"
+#' @param service "ncss"
+#' @param dataset path dataset dari katalog
+#' @param var nama variabel
+#' @param query tambahan query
+#'
+#' @import easyNCDF
+#'
+#' @return kueri NCSS
+#'
+#' @export
+
+query_ncss <- function(
+  host = "thredds.ucar.edu",
+  context = "thredds",
+  service = "ncss",
+  dataset = "grib/NCEP/GEFS/Global_1p0deg_Ensemble/members/GEFS_Global_1p0deg_Ensemble_20190213_0000.grib2",
+  var = "Total_precipitation_surface_6_Hour_Accumulation_ens",
+  query = "&north=6&west=90&east=141&south=-11&temporal=all"
+) {
+  res <- glue::glue("http://{host}/{context}/{service}/{dataset}?var={var}{query}")
+
+  return(res)
+}
