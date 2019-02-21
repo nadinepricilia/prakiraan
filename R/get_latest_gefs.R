92ecb397b007e8f7f19e2a7f68f1e7522ca78ba6
Nadine Pricilia <nadinepricilia@gmail.com>
2019-02-21 20:51:35 +0700

    Added GFS downloader.

--- R/get_latest_gefs.R
+++ R/get_latest_gefs.R
@@ -19,7 +19,7 @@
 get_latest_gefs <- function(
   var = "Total_precipitation_surface_6_Hour_Accumulation_ens",
   query = "&north=6&west=90&east=141&south=-11&temporal=all",
-  destfile = "latest.nc"
+  destfile = "latest_gefs.nc"
 ){
   dataset <- 
     read_html("http://thredds.ucar.edu/thredds/catalog/grib/NCEP/GEFS/Global_1p0deg_Ensemble/members/latest.html") %>% 
@@ -28,5 +28,5 @@ get_latest_gefs <- function(
   gefs_url <- glue::glue("http://thredds.ucar.edu/thredds/ncss/grib/NCEP/GEFS/Global_1p0deg_Ensemble/members/{dataset}?var={var}{query}")
   
   download.file(gefs_url, destfile = destfile)
-  invisible(query)
+  invisible(gefs_url)
 }
---
+++ R/get_latest_gfs.R
@@ -0,0 +1,32 @@
+#' Mengunduh GFS 0.25 Terkini
+#'
+#' Fungsi untuk mengunduh GFS 0.25 terkini dengan memanfaatkan NetCDF Subset Service
+#'
+#' @param var nama variabel
+#' @param query tambahan query
+#' @param destfile destinasi berkas GFS unduhan
+#'
+#' @importFrom xml2 read_html
+#' @importFrom rvest html_table
+#' @importFrom glue glue
+#' @importFrom magrittr %>%
+#' @importFrom utils download.file
+#' 
+#' @return berkas GFS dan URL NCSS
+#'
+#' @export
+
+get_latest_gfs <- function(
+  var = "Total_precipitation_surface_Mixed_intervals_Accumulation",
+  query = "&north=6&west=90&east=141&south=-11&temporal=all",
+  destfile = "latest_gfs.nc"
+){
+  dataset <- 
+    read_html("http://thredds.ucar.edu/thredds/catalog/grib/NCEP/GFS/Global_0p25deg/latest.html") %>% 
+    html_table() %>% 
+    `[[`(c(1,1))
+  gfs_url <- glue::glue("http://thredds.ucar.edu/thredds/ncss/grib/NCEP/GFS/Global_0p25deg/{dataset}?var={var}{query}")
+  
+  download.file(gfs_url, destfile = destfile)
+  invisible(gfs_url)
+}
